b4891ff8b16919efcb572551e9f8f284af0830ff
fhb <18979687252@163.com>
2018-07-11 18:03:59 +0800

    增加注释

--- src/index.js
+++ src/index.js
@@ -14,3 +14,21 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root')
 )
+/*
+  每个组件render函数内有必须包含一个根元素；
+  react-redux 是将独立的redux和react结合起来，react-redux核心分为两个部分：Provider、connect
+  Provider:将redux中store传递到内部的其他组件，让组件可以享有store,并提供对state的更新
+  connect:共有四个参数，但我这里只说基本的两个，mapStateToProps和mapDispatchToProps
+  1.mapStateToProps把状态绑定到组件的属性当中。我们定义的state对象有哪些属性，在我们组件的props都可以查阅和获取。
+    const mapStateToProps = (state, ownProps) => {
+      return {tableIndex:state.tableIndex}
+    }
+  2.mapDispatchToProps：return一个dispatch函数，将该方法绑定到属性上，我们同样可以在props查看和调用。
+
+  const mapDispatchToProps = (dispatch, ownProps) => {
+    return {
+        changeActive:(args)=>dispatch({type:"changeTable",data:args})
+    }
+  }
+
+*/
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
 import rootReducer from '../reducers/rootReducer'
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
-
 const enhancer = compose(
     applyMiddleware(thunk)
 )
 export default createStore(rootReducer, enhancer)
+
+/*
+ 创建store
+ redux-thunk中间件帮助我们统一异步和同步的action的调用方式。
+*/
